2a9824325584e1f510b6a1c72dc58712e8bf02d4
valiticat <106557000+valiticat@users.noreply.github.com>
2023-10-19 09:03:24 +0000

    eps

--- pages/JNJ.py
+++ pages/JNJ.py
@@ -6,7 +6,15 @@ st.header('Johnson & Johnson (JNJ)')
 st.subheader('')
 
 jnj = yf.Ticker("JNJ")
-st.text(jnj.earnings_dates)
+# EPS
+eps_df = jnj.earnings_dates
+latest_eps = eps_df[eps_df['Reported EPS'].notnull()].head(1)
+latest_eps
+#f"Latest EPS reported: {latest_eps['Reported EPS']} USD. Estimate: USD"
+#st.text(pd.to_datetime(ids).year)
+#test = eps_df[pd.to_datetime(eps_df.index.dt.date) == '2023-10-17']
+#test
+#pd.to_datetime(mygrafic['Дата'].dt.date)
 
 # get historical market data
 hist = jnj.history(period="1mo")
