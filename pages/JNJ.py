74a42ae8e61dc4c9d018b1a993f2b3e0736105a2
valiticat <106557000+valiticat@users.noreply.github.com>
2023-10-19 02:38:48 +0000

    jnj

--- pages/JNJ.py
+++ pages/JNJ.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import yfinance as yf
+import pandas as pd
+
+st.title('Johnson & Johnson (JNJ)')
+st.subtitle('')
 
 jnj = yf.Ticker("JNJ")
 
 # get historical market data
 hist = jnj.history(period="1mo")
+st.line_chart(hist)
